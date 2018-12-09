f11c88cbb5b56e6e8f03b007e9c27f525e42821a
TeeNik <teenik3315@gmail.com>
2018-12-10 01:32:04 +0300

    constructor

---
+++ Assets/Scripts/System/Assert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using DG.Tweening;
+
+public class Assert {
+
+    	public static bool Inv(bool condition, params object[] args)
+	{
+		if (!condition)
+		{
+			var sb = new StringBuilder("Assertion: ");
+
+			if (args.Length > 0)
+			{
+				sb.Append(args[0]);
+				for (var i = 1; i < args.Length; ++i)
+				{
+					sb.Append(" ");
+					sb.Append(args[i] != null ? args[i].ToString() : "NULL");
+				}
+			}
+
+			throw new Exception(sb.ToString());
+		}
+		return condition;
+	}
+
+}
---
+++ Assets/Scripts/System/Constructor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Generator;
+using UnityEngine;
+
+public class Constructor : MonoBehaviour
+{
+
+    public List<BookElement> Elements;
+
+    public void CreateItem(XmlNode node, Transform parent)
+    {
+        var prototype = Elements.Find()
+        var element = Instantiate()
+    }
+
+}
