7bb49a084cb68905f1221146f3e7dac0d427cd01
TeeNik <teenik3315@gmail.com>
2019-01-03 21:02:07 +0300

    refactor

--- Assets/Scripts/System/BookResources.cs
+++ Assets/Scripts/System/BookResources.cs
@@ -4,7 +4,7 @@ public class BookResources
 {
     public T Get<T>(string name) where T : Object 
     {
-        var obj = Resources.Load<T>("BookResorces/" + name);
+        var obj = Resources.Load<T>("BookResources/" + name);
         Assert.Inv(obj != null, "obj != null", name);
         return obj;
     }
