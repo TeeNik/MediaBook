7754a9b106b303dfbbfe77486abb7170e0aa8464
TeeNik <teenik3315@gmail.com>
2019-01-04 22:10:45 +0300

    command subject

---
+++ Assets/Scripts/Communication/CommandSubject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate void Callback<T>(T arg1);
+
+public class CommandSubject 
+{
+    public Dictionary<Type, Delegate> _eventTable = new Dictionary<Type, Delegate>();
+
+    public void Subscribe<T>(Callback<T> handler)
+    {
+        Type eventType = typeof(T);
+        if (!_eventTable.ContainsKey(eventType))
+        {
+            _eventTable.Add(eventType, null);
+        }
+        _eventTable[eventType] = (Callback<T>)_eventTable[eventType] + handler;
+    }
+
+    private void RemoveListener<T>(Delegate handler)
+    {
+        Type eventType = typeof(T);
+
+        if (_eventTable.ContainsKey(eventType))
+        {
+            _eventTable[eventType] = Delegate.Remove(_eventTable[eventType], handler);
+            if (_eventTable[eventType] == null)
+            {
+                _eventTable.Remove(eventType);
+            }
+        }
+    }
+
+    public void Broadcast<T>(T arg)
+    {
+        Type eventType = typeof(T);
+        Delegate d;
+        if (_eventTable.TryGetValue(eventType, out d))
+        {
+            Callback<T> callback = d as Callback<T>;
+            callback?.Invoke(arg);
+        }
+    }
+
+}
