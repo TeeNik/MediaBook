fb977bf382afe542be4dbe05547680826e93d942
TeeNik <teenik3315@gmail.com>
2019-02-18 01:38:30 +0300

    auth

---
+++ Assets/Scripts/System/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuthController : MonoBehaviour
+{
+
+    public string Name { get; private set; }
+    public string Group { get; private set; }
+
+    public void Auth(string name, string group)
+    {
+
+    }
+
+}
