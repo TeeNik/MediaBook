494ab31668278a50cbef7f723786fde74d506b98
TeeNik <teenik3315@gmail.com>
2019-02-26 07:48:14 +0300

    rotate 3d

--- Assets/Scripts/Windows/ModelViewWindow.cs
+++ Assets/Scripts/Windows/ModelViewWindow.cs
@@ -1,15 +1,28 @@
 ﻿using UIWindow;
+using UnityEngine;
 using UnityEngine.EventSystems;
 public class ModelViewWindow : Window, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Transform Object;
+    private float _sensitivity = 0.5f;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation = Vector3.zero;
+    private bool _isRotating;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         print("begin drag");
     }
 
+    float rotSpeed = 300;
     public void OnDrag(PointerEventData eventData)
     {
         print("drag");
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        //float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+        Object.Rotate(Vector3.up, -rotX);
+        //Object.Rotate(Vector3.right, rotY);
     }
 
     public void OnEndDrag(PointerEventData eventData)
