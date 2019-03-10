ac3b823985a472bd29a06037566ede8d314e4767
TeeNik <teenik3315@gmail.com>
2019-03-10 20:41:43 +0300

    toggle

---
+++ Assets/Scripts/Constructor/Test/QuestionPanelElement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Generator
+{
+    public class QuestionPanelButton : BookElement
+    {
+        public override string Type => ElementTag.QuestionPanel;
+
+        [SerializeField] private TMPro.TMP_Text _question;
+        [SerializeField] private Transform _container;
+        [SerializeField] private ToggleGroup group;
+
+        private List<AnswerToggleElement> _toggles;
+        private string _answerId;
+
+        public override void Init(XmlNode content)
+        {
+            var constructor = DataLayer.Instance.Constructor;
+            _toggles = new List<AnswerToggleElement>();
+            _answerId = content.Attributes["answer"].InnerText;
+
+            foreach (XmlNode node in content)
+            {
+                AnswerToggleElement item = constructor.CreateItem(node, _container) as AnswerToggleElement;
+                item.SetToggleGroup(group);
+            }
+        }
+
+        public bool CheckAnswer()
+        {
+            return _toggles.First(t => t.GetToggleValue()).Id == _answerId;
+        }
+    }
+}
