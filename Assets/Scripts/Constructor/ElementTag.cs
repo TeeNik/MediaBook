ad6893b817e279750e0e079e10494f1213f213ac
TeeNik <teenik3315@gmail.com>
2019-03-10 20:41:26 +0300

    element tag

---
+++ Assets/Scripts/Constructor/ElementTag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generator
+{
+    public class ElementTag
+    {
+        public const string QuestionPanel = "questionPanel";
+        public const string ImageContainer = "image_container";
+        public const string AnswerToggle = "answer_toggle";
+    }
+}
+
